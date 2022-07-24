18c052f25a4d735452021e7287f28651387aabc2
metiftikci <metiftikci@hotmail.com>
2022-07-24 20:09:22 +0300

    Fix build docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -85,7 +85,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/getting-started',
               },
             ],
           },
@@ -109,10 +109,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
