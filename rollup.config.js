b7c5f90580f7a5e57ac7d38f701db2b409c9ae9e
metiftikci <metiftikci@hotmail.com>
2025-09-22 20:15:41 +0300

    build: specify entry point and add types create process

--- rollup.config.js
+++ rollup.config.js
@@ -4,26 +4,35 @@ import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dev from "rollup-plugin-dev";
+import dts from "rollup-plugin-dts";
 import css from "rollup-plugin-import-css";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    file: "dist/ankareport.js",
-    name: "AnkaReport",
-    format: "iife",
-    exports: "named",
-    inlineDynamicImports: true,
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/ankareport.js",
+      name: "AnkaReport",
+      format: "iife",
+      exports: "named",
+      inlineDynamicImports: true,
+    },
+    plugins: [
+      typescript(),
+      commonjs(),
+      resolve({ browser: true }),
+      json(),
+      css({
+        output: "ankareport.css",
+      }),
+      image(),
+      dev({ dirs: ["dev", "dist"] }),
+    ],
   },
-  plugins: [
-    typescript(),
-    commonjs(),
-    resolve({ browser: true }),
-    json(),
-    css({
-      output: "ankareport.css",
-    }),
-    image(),
-    dev({ dirs: ["dev", "dist"] }),
-  ],
-};
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    external: [/.css/],
+    plugins: [dts()]
+  }
+];
